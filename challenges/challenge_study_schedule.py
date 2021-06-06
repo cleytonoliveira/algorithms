098fd49161e81db1aacec77616305f57c7d46d11
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-06-06 19:23:45 -0300

    feat: implementation study_schedule with algorithm search

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(start_time, end_time, target_time):
     """ Faça o código aqui. """
+    if not target_time or not start_time:
+        return 0
+
+    number_of_students_by_time = 0
+    for index, start_study_time in enumerate(start_time):
+        if start_study_time <= target_time <= end_time[index]:
+            number_of_students_by_time += 1
+
+    return number_of_students_by_time
