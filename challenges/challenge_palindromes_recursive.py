9035cf710db44bdadbac482c0867b2c1095ffb97
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-06-06 11:18:51 -0300

    feat: implementation of palindrome recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
-def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+def is_palindrome_recursive(word: str, low: int, high: int) -> bool:
+    """Identifica palavras ou frases palíndromas de forma recursiva"""
+    if word == "" or word[low] != word[high]:
+        return False
+    if low == high:
+        return True
+    if (low < high + 1):
+        return is_palindrome_recursive(word, low + 1, high - 1)
+    return True
