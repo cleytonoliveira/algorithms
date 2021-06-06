0b585facaa335b74485d637d3c382a2b3cb8da20
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-06-06 18:42:18 -0300

    feat: implement anagram with merged sort

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,37 @@
-def is_anagram(first_string, second_string):
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+    return merge(left, right, array.copy())
+
+
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
+
+def is_anagram(first_string: str, second_string: str) -> bool:
     """ Faça o código aqui. """
+    first_string_list = list(first_string)
+    first_string_sorted = merge_sort(first_string_list)
+    second_string_list = list(second_string)
+    second_string_sorted = merge_sort(second_string_list)
+
+    return first_string_sorted == second_string_sorted
